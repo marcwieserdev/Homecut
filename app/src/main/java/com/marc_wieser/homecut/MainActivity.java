637305f3f7c172717b943805ca50fb34ea832627
Marc <marc.wieser@wobility.fr>
2016-11-13 16:57:35 +0100

    Lints issues ; adapt to Nexus 5 ; adapt to API 24

--- app/src/main/java/com/marc_wieser/homecut/MainActivity.java
+++ app/src/main/java/com/marc_wieser/homecut/MainActivity.java
@@ -9,22 +9,39 @@
  */
 package com.marc_wieser.homecut;
 
+import android.app.Activity;
+import android.app.Service;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PermissionGroupInfo;
+import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.storage.StorageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.content.PermissionChecker;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.marc_wieser.homecut.services.ShareService;
 
+import java.security.Permission;
+import java.security.Permissions;
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final int REQUEST_RES = 4242;
     private void logIntent(Intent intent){
         Bundle bundle = intent.getExtras();
         Set<String> categories = intent.getCategories();
         String type = intent.getType();
+        String action = intent.getAction();
+        if (action != null)
+            Log.d("DUMP-ACTION", action);
         if (type != null)
             Log.d("DUMP-TYPE", type);
         if (intent.getDataString() != null)
@@ -45,11 +62,36 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_RES){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Intent intent = getIntent();
+                intent.setClass(this, ShareService.class);
+                startService(intent);
+                finish();
+                return;
+            }
+            Toast.makeText(this, R.string.error_permission, Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
-        if (intent != null && Intent.ACTION_SEND.equals(intent.getAction())){
-            logIntent(intent);
+        if (intent != null && (Intent.ACTION_SEND.equals(intent.getAction()) || Intent.ACTION_SEND_MULTIPLE.equals(intent.getAction()))){
+            if (BuildConfig.DEBUG){
+                logIntent(intent);
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (checkSelfPermission("android.permission.READ_EXTERNAL_STORAGE") != PackageManager.PERMISSION_GRANTED){ //TODO : replace by constant
+                    requestPermissions(new String[]{"android.permission.READ_EXTERNAL_STORAGE"}, REQUEST_RES);
+                    return;
+                }
+            }
             intent.setClass(this, ShareService.class);
             startService(intent);
             finish();
--- app/src/main/java/com/marc_wieser/homecut/services/ShareService.java
+++ app/src/main/java/com/marc_wieser/homecut/services/ShareService.java
@@ -10,17 +10,31 @@
 package com.marc_wieser.homecut.services;
 
 import android.app.IntentService;
+import android.app.Service;
 import android.content.Intent;
 import android.database.Cursor;
+import android.database.DatabaseUtils;
 import android.net.Uri;
+import android.os.Build;
+import android.os.storage.StorageManager;
+import android.provider.DocumentsContract;
+import android.provider.DocumentsProvider;
+import android.provider.MediaStore;
 import android.provider.OpenableColumns;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.webkit.MimeTypeMap;
 
+import com.marc_wieser.homecut.BuildConfig;
 import com.marc_wieser.homecut.R;
+
+import java.io.File;
+import java.net.URI;
+import java.util.ArrayList;
 
 
 public class ShareService extends IntentService {
-    public final static String ACTION_CREATE_SHARING = "create_share";
-
+    private final String LOG_TAG = getClass().getSimpleName();
     public ShareService() {
         super("ShareService");
     }
@@ -33,36 +47,41 @@ public class ShareService extends IntentService {
             case Intent.ACTION_SEND:
                 handleActionSend(intent);
                 break;
+            case Intent.ACTION_SEND_MULTIPLE:
+                handleActionSendMultiple(intent);
+                break;
             default:
                 throw new UnsupportedOperationException("Unsupported action");
         }
     }
 
-    private void handleActionSend(Intent intent){
-        String name, type;
-        Uri stream = intent.getParcelableExtra(Intent.EXTRA_STREAM);
-        String[] projection = {
-                OpenableColumns.DISPLAY_NAME
-        };
-        if (stream == null){
-            //TODO : put error message
+    private void handleActionSendMultiple(Intent intent){
+        ArrayList<Uri> streams = intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
+        if (streams == null)
             return;
+        for (Uri stream : streams) {
+            if (stream == null){
+                if (BuildConfig.DEBUG)
+                    Log.e(LOG_TAG, "Stream null");
+                continue;
+            }
+            String name = getShortcutName(stream);
+            if (name == null){
+                if (BuildConfig.DEBUG)
+                    Log.i(LOG_TAG, "Unsupported device, stream Uri : " + stream);
+                continue;
+            }
+
+            String type = getShortcutType(stream);
+            if (BuildConfig.DEBUG)
+                Log.d(LOG_TAG, type);
+            int resourceID = getShortcutIcon(type);
+            createShortcut(name, resourceID, stream, type);
         }
-        Cursor file = getContentResolver().query(stream, projection, null, null, null);
-        if (file == null || !file.moveToFirst()){
-            //TODO : put error message
-            return;
-        }
 
-        name = intent.getStringExtra(Intent.EXTRA_TITLE);
-        name = name == null || name.isEmpty() ? file.getString(0) : name;
-        file.close();
-        type = intent.getType();
+    }
 
-        Intent shortcutIntent = new Intent(Intent.ACTION_VIEW);
-        shortcutIntent.setType(type);
-        shortcutIntent.setData(stream);
-
+    private int getShortcutIcon(String type){
         int resourceID;
         if (type.startsWith("pdf") || type.endsWith("pdf")){
             resourceID = R.drawable.ic_pdf;
@@ -75,17 +94,122 @@ public class ShareService extends IntentService {
         } else {
             resourceID = R.drawable.ic_file;
         }
+        return resourceID;
+    }
 
-        Intent chooser = Intent.createChooser(shortcutIntent, "Open With");
+    @Nullable
+    private String getShortcutName(Uri stream){
+        final String[] projection = {
+                OpenableColumns.DISPLAY_NAME
+        };
+        String name = null;
+        if (stream.toString().startsWith("content:")){
+            Cursor file = getContentResolver().query(stream, projection, null, null, null);
+            if (file == null || !file.moveToFirst()){
+                if (BuildConfig.DEBUG)
+                    Log.e(LOG_TAG, "file resolving failed");
+                return null;
+            }
+            name = file.getString(0);
+            file.close();
+        } else if (stream.toString().startsWith("file:")){
+            File file = new File(URI.create(stream.toString()));
+            name = file.getName();
+        }
+        return name;
+    }
+
+    private String getShortcutType(Uri stream){
+        String type = null;
+
+        if (stream.toString().startsWith("content:")){
+            type = getContentResolver().getType(stream);
+        } else if (stream.toString().startsWith("file:")){
+            type = MimeTypeMap.getFileExtensionFromUrl(stream.toString());
+            type = MimeTypeMap.getSingleton().getMimeTypeFromExtension(type);
+        }
+        return type;
+    }
+
+    @Nullable
+    private Uri getContentUri(String filePath, String mime){
+        if (mime.startsWith("image") || mime.endsWith("image")){
+            Cursor file = getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, new String[]{MediaStore.Images.Media._ID}, MediaStore.Images.Media.DATA+"=?", new String[]{filePath}, null);
+            if (file != null && file.moveToFirst()){
+                long id = file.getLong(0);
+                file.close();
+                return Uri.withAppendedPath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, String.valueOf(id));
+            }
+        } else if (mime.startsWith("video") || mime.endsWith("video")){
+            Cursor file = getContentResolver().query(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, new String[]{MediaStore.Video.Media._ID}, MediaStore.Video.Media.DATA+"=?", new String[]{filePath}, null);
+            if (file != null && file.moveToFirst()){
+                long id = file.getLong(0);
+                file.close();
+                return Uri.withAppendedPath(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, String.valueOf(id));
+            }
+        } else if (mime.startsWith("audio") || mime.endsWith("audio")){
+            Cursor file = getContentResolver().query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, new String[]{MediaStore.Audio.Media._ID}, MediaStore.Audio.Media.DATA+"=?", new String[]{filePath}, null);
+            if (file != null && file.moveToFirst()){
+                long id = file.getLong(0);
+                file.close();
+                return Uri.withAppendedPath(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, String.valueOf(id));
+            }
+        }
+        return null;
+    }
+
+    private void createShortcut(String name, int resourceID, Uri stream, String type){
+        Intent shortcutIntent = new Intent();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            shortcutIntent.setAction(Intent.ACTION_QUICK_VIEW);
+        } else {
+            shortcutIntent.setAction(Intent.ACTION_VIEW);
+        }
+        shortcutIntent.setType(type);
+        if (stream.toString().startsWith("file:")){
+            File file = new File(URI.create(stream.toString()));
+            Uri newStream = getContentUri(file.getAbsolutePath(), type);
+            if (newStream != null)
+                stream = newStream;
+        }
+        shortcutIntent.setData(stream);
+        if (BuildConfig.DEBUG)
+            Log.d(LOG_TAG, String.valueOf(stream));
         Intent addIntent = new Intent();
-        addIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, chooser);
+        addIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
         addIntent.putExtra(Intent.EXTRA_SHORTCUT_NAME, name);
         addIntent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE,
                 Intent.ShortcutIconResource.fromContext(getApplicationContext(),
                         resourceID));
-
         addIntent.setAction("com.android.launcher.action.INSTALL_SHORTCUT");
         getApplicationContext().sendBroadcast(addIntent);
+    }
+
+    private void handleActionSend(Intent intent){
+        String name, type;
+        Uri stream = intent.getParcelableExtra(Intent.EXTRA_STREAM);
+        if (stream == null){
+            if (BuildConfig.DEBUG)
+                Log.e(LOG_TAG, "Stream null");
+            return;
+        }
+
+        name = intent.getStringExtra(Intent.EXTRA_TITLE);
+        if (name == null || name.isEmpty()){
+           name = getShortcutName(stream);
+            if (name == null || name.isEmpty()){
+                if (BuildConfig.DEBUG)
+                    Log.i(LOG_TAG, "Unsupported device, stream Uri : " + stream);
+                return;
+            }
+        }
+        type = intent.getType();
+        if (type == null || type.isEmpty())
+            type = getShortcutType(stream);
+        int resourceID = getShortcutIcon(type);
+        createShortcut(name, resourceID, stream, type);
+        if (BuildConfig.DEBUG)
+            Log.i(LOG_TAG, "Shortcut creation success");
     }
 
 }
